0e5675086f295d7a9f2324282e1ee53f047f4bbb
Fapcod <Fapcod@protonmail.com>
2021-11-29 14:49:58 -0500

    modificacion de mensaje de gmail

--- resources/views/emails/aviso.blade.php
+++ resources/views/emails/aviso.blade.php
@@ -7,12 +7,10 @@
     <title>Aviso Servicio tecnico</title>
 </head>
 <body>
-    <h1>Hola Amigo <b> {{ $contacto->nombredueno }} {{ $contacto->apellidodueno }} </b></h1>
-    <p>Te esta hablando el servicio tecnico de compusolution <br>
-    Te quiero decir que tu equipo <b> {{ $contacto->marcaequipo }} {{ $contacto->modeloequipo }} </b> ya esta arreglado y puedes pasar por el cuando desees de Lunes a Viernes de 8am a 6pm.
+    <h1>Hola<b> {{ $contacto->nombredueno }} {{ $contacto->apellidodueno }} </b></h1>
+    <p>Te contactamos desde el Servicio Técnico de Compusolution. <br>
+    Tu equipo <b> {{ $contacto->marcaequipo }} {{ $contacto->modeloequipo }} </b>se encuentra en estado operativo y listo para su recojo de Lunes a Viernes de 8am a 6pm.
     </p>
     <p>En el caso de no poder a esa hora, puedes llamar al 987543243 o enviar un mensaje al whatsap para poder coordinar la entrega.</p>
-
-
 </body>
 </html>
